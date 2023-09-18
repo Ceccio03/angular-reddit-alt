66b326e6210a2ea33527873b3d6fbe6bd3f922a6
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-09-18 11:56:40 +0200

    new repo

---
+++ src/app/services/reddit.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { map, tap } from 'rxjs';
+import { Post } from '../model/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RedditService {
+
+  constructor(private http: HttpClient) { }
+
+  getRedditPosts() {
+    return this.http.get<Post>('https://www.reddit.com/r/italy/new.json').pipe(
+      // tap(redditObj => console.log('oggetto completo', redditObj)),
+      map((redditObj: any) => redditObj.data),
+      // tap(redditData => console.log('oggetto data', redditData)),
+      map(redditData => redditData.children),
+      // tap(redditChildren => console.log('children', redditChildren)),
+      map(redditChildren => redditChildren.map((child: any) => child.data))
+    );
+  }
+}
